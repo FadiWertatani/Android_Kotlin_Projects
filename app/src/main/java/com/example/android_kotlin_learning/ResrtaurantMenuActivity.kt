aab222e4c523e2464e1833c2c4fb745daa0c8360
FadiWertatani <wertatani@isikef.u-jendouba.tn>
2022-05-27 10:25:30 +0100

    initial commit

---
+++ app/src/main/java/com/example/android_kotlin_learning/ResrtaurantMenuActivity.kt
@@ -0,0 +1,28 @@
+package com.example.android_kotlin_learning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import com.example.android_kotlin_learning.databinding.ActivityResrtaurantMenuBinding
+
+class ResrtaurantMenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResrtaurantMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResrtaurantMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnOrder.setOnClickListener {
+            val checkedRadioButtonId = binding.rgBurgers.checkedRadioButtonId
+            val burger = findViewById<RadioButton>(checkedRadioButtonId)
+            val cheese = binding.chbCheese.isChecked
+            val onions = binding.chbOnion.isChecked
+            val salad = binding.chbSalad.isChecked
+            val totalOrder = "You order a ${burger.text} burger with:"+
+                    if (cheese == true) {"\n cheese"}else{""}+
+                    if (salad == true)  {"\n salad"}  else{""}+
+                    if (onions == true) {"\n onion"} else{""}
+            binding.Result.text = totalOrder
+        }
+    }
+}
